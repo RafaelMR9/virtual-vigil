a518572b06480a6aaed9be68e03f927bc406b6f8
RafaelMR9 <rafaelmedeirosa98@gmail.com>
2024-02-14 17:30:00 -0300

    Navbar responsiviness

--- src/components/sections/Nav.jsx
+++ src/components/sections/Nav.jsx
@@ -57,50 +57,48 @@ export default function Nav() {
   }
 
   return (
-    <nav className="bg-slate-800 rounded-full shadow-xl flex justify-between items-center fixed top-0 left-0 right-0 px-8 py-4 mt-4 mx-10 z-[999]">
+    <nav className="flex rounded-lg items-center justify-between flex-wrap bg-slate-800 shadow-nav fixed top-0 left-0 right-0 z-[999] px-8 py-4 mt-4 mx-10">
       <Link 
         href="#home"
         onClick={(e) => handleScrollToSection(e, 'home')} 
-        className="flex items-center gap-1 font-bold text-2xl">
+        className="flex flex-shrink-0 items-center gap-1 font-bold text-2xl mr-0 lg:mr-[20rem]">
         <FaShieldVirus /> Virtual Vigil
       </Link>
-      <button 
-        type="button" 
-        className="inline-flex items-center p-2 w-8 h-8 justify-center text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" 
-        aria-controls="navbar-default" 
-        aria-expanded={isMenuOpen}
-        onClick={() => setIsMenuOpen(!isMenuOpen)}
-      >
-        <span className="sr-only">Open main menu</span>
-        <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
-        </svg>
-      </button>
-      <div className={`lg:flex gap-12 mt-1 font-semibold ${isMenuOpen ? 'flex flex-col' : 'hidden'}`} id="navbar-default">
-        <Link 
-          href="#home" 
-          onClick={(e) => handleScrollToSection(e, 'home')}
-          className={`relative after:bg-violet-400 after:absolute after:h-1 after:w-0 after:bottom-0 after:left-0 after:transition-all after:duration-300 cursor-pointer ${activeSection === 'home' ? 'after:w-full' : 'hover:after:w-full'}`}>
-          Home
-        </Link>
-        <Link 
-          href="#services" 
-          onClick={(e) => handleScrollToSection(e, 'services')}
-          className={`relative after:bg-violet-400 after:absolute after:h-1 after:w-0 after:bottom-0 after:left-0 after:transition-all after:duration-300 cursor-pointer ${activeSection === 'services' ? 'after:w-full' : 'hover:after:w-full'}`}>
-          Serviços
-        </Link>
-        <Link 
-          href="#plans" 
-          onClick={(e) => handleScrollToSection(e, 'plans')}
-          className={`relative after:bg-violet-400 after:absolute after:h-1 after:w-0 after:bottom-0 after:left-0 after:transition-all after:duration-300 cursor-pointer ${activeSection === 'plans' ? 'after:w-full' : 'hover:after:w-full'}`}>
-          Planos
-        </Link>
-        <Link 
-          href="#contact" 
-          onClick={(e) => handleScrollToSection(e, 'contact')}
-          className={`relative after:bg-violet-400 after:absolute after:h-1 after:w-0 after:bottom-0 after:left-0 after:transition-all after:duration-300 cursor-pointer ${activeSection === 'contact' ? 'after:w-full' : 'hover:after:w-full'}`}>
-          Contato
-        </Link>
+      <div className="block lg:hidden">
+        <button
+          className="flex items-center px-3 py-2 border rounded border-white hover:text-violet-400 hover:border-violet-400"
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+        >
+          <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+        </button>
+      </div>
+      <div className={`w-full ${isMenuOpen ? 'hidden' : 'block'} flex-grow lg:flex lg:items-center lg:w-auto`}>
+        <div className="lg:flex-grow">
+          <Link 
+            href="#home" 
+            onClick={(e) => handleScrollToSection(e, 'home')}
+            className='block mt-4 lg:inline-block lg:mt-0 text-center lg:text-start mr-0 lg:mr-12'>
+            <span className={`relative after:bg-violet-400 after:absolute after:h-1 after:w-0 after:-bottom-[0.25rem] after:left-0 after:transition-all after:duration-300 ${activeSection === 'home' ? 'after:w-full' : 'hover:after:w-full'}`}>Home</span>
+          </Link>
+          <Link 
+            href="#services" 
+            onClick={(e) => handleScrollToSection(e, 'services')}
+            className='block mt-4 lg:inline-block lg:mt-0 text-center lg:text-start mr-0 lg:mr-12'>
+            <span className={`relative after:bg-violet-400 after:absolute after:h-1 after:w-0 after:-bottom-[0.25rem] after:left-0 after:transition-all after:duration-300 ${activeSection === 'services' ? 'after:w-full' : 'hover:after:w-full'}`}>Serviços</span>
+          </Link>
+          <Link 
+            href="#plans" 
+            onClick={(e) => handleScrollToSection(e, 'plans')}
+            className='block mt-4 lg:inline-block lg:mt-0 text-center lg:text-start mr-0 lg:mr-12'>
+            <span className={`relative after:bg-violet-400 after:absolute after:h-1 after:w-0 after:-bottom-[0.25rem] after:left-0 after:transition-all after:duration-300 ${activeSection === 'plans' ? 'after:w-full' : 'hover:after:w-full'}`}>Planos</span>
+          </Link>
+          <Link 
+            href="#contact" 
+            onClick={(e) => handleScrollToSection(e, 'contact')}
+            className='block mt-4 lg:inline-block lg:mt-0 text-center lg:text-start'>
+            <span className={`relative after:bg-violet-400 after:absolute after:h-1 after:w-0 after:-bottom-[0.25rem] after:left-0 after:transition-all after:duration-300 ${activeSection === 'contact' ? 'after:w-full' : 'hover:after:w-full'}`}>Contato</span>
+          </Link>
+        </div>
       </div>
     </nav>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,9 @@ module.exports = {
           "10px 10px 8px rgba(255,255, 255, 0.2)"
         ]
       },
+      boxShadow: {
+        'nav': '0px 0px 10px 10px rgba(0,0,0,0.1)',
+      }
     },
   },
   plugins: [],
